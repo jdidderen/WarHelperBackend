bc1ac9111d466418b8622b589496c81654961c8a
Didderen Jérémy <didderen.jeremy@jdi.me>
2020-08-02 19:43:02 +0200

    static

--- warhammer_fire_backend/settings.py
+++ warhammer_fire_backend/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 REST_SESSION_LOGIN = True
