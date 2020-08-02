98d50887997b6f491c7e66e75b5345d86c1f5c7b
Didderen Jérémy <didderen.jeremy@jdi.me>
2020-08-02 23:34:52 +0200

    stratagem army serializer

--- WFB_Stratagem/serializer.py
+++ WFB_Stratagem/serializer.py
@@ -2,11 +2,19 @@ from rest_framework import serializers
 from .models import Stratagem,StratagemPhase
 
 class StratagemSerializer(serializers.ModelSerializer):
+    army = serializers.SerializerMethodField('get_army')
+
 
     class Meta:
         model = Stratagem
         fields = ('id',
-                  'name','description','cost','type','army_id','phase_id')
+                  'name','description','cost','type','army_id','phase_id','army')
+
+    def get_army(self, obj):
+        if obj.army_id:
+            return obj.army_id.name
+        else:
+            return ''
 
 class StratagemPhaseSerializer(serializers.ModelSerializer):
 
